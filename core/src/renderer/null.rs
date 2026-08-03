5d515626a742c305e10e77da3ba34fc523ef1668
Cristian Brinza <cristianb@gmail.com>
2026-08-03 14:39:10 +0300

    Add search text to Editor

--- core/src/renderer/null.rs
+++ core/src/renderer/null.rs
@@ -217,6 +217,10 @@ impl text::Editor for () {
     ) {
     }
 
+    fn search(&mut self, _text: &str, _forwards: bool, _wrap_around: bool) -> bool {
+        false
+    }
+
     fn text_size(&self) -> Pixels {
         Pixels(0.0)
     }
